853dd04b3764dc7791eda8994f8f8ecedabb2318
xkrieg <xkrieg@gmail.com>
2017-07-11 18:47:07 +0900

    Add pdfunite information

--- pdfs21.py
+++ pdfs21.py
@@ -1,13 +1,25 @@
+#Import required functions
 from PyPDF2 import PdfFileMerger, PdfFileReader
+import os
 
+#Create list from directory of pdfs
 pdfList = []
 for root, subdirs, files in os.walk('_output/'):
-  for file in files:
-    if os.path.splitext(file)[1].lower() in ('.pdf'):
-      pdfList.append(os.path.join(root, file))
+  for myfile in files:
+    if os.path.splitext(myfile)[1].endswith('.pdf'):
+      pdfList.append(os.path.join(root, myfile))
 
+#Check list
+print(pdfList)
+
+#Merge pdfs
 merger = PdfFileMerger()
 for filename in pdfList:
-    merger.append(PdfFileReader(file(filename, 'rb')))
+    merger.append(PdfFileReader(file(filename.strip(), 'rb')))
 
+#Save file
 merger.write("allReports.pdf")
+
+#Better alternative
+#brew install poppler
+#pdfunite *.pdf all.pdf
